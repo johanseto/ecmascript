9f86d089bb9621eb3fc18a013a2d522560d7ccd0
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-12 21:41:06 -0500

    more aspects, join elements..var,let,const

--- src/es6/index.js
+++ src/es6/index.js
@@ -31,3 +31,35 @@ console.log(epicPhrase)
 //templates
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
+
+
+let lorem = "una frase epica   \n"
++ "otra frase epica"
+
+console.log(lorem)
+
+let lorem2 = `otra muy crazy epica 
+ahora otra frase epica`;
+
+console.log(lorem2)
+
+let person ={
+    'name':'oscar',
+    'age':'32',
+    'country':'MX'
+}
+
+console.log(person.name,person.age)
+
+let {name,age,country} = person; //desestrcuture the elements ma niggy, omg.
+console.log(name,age,country);
+
+
+//Join elemnts
+let team1 = ['oscar','Julian','Ricardo']
+let team2 = ['Valeria','Yesica','Camila']
+
+let education = ['David',...team1,...team2]
+
+console.log(education)
+
