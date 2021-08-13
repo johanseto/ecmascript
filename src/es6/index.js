b18d98c7d820931f818a61aede2ebefb7699d23d
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-12 22:11:05 -0500

    arrow function and helloPromiseWorld.

--- src/es6/index.js
+++ src/es6/index.js
@@ -63,3 +63,76 @@ let education = ['David',...team1,...team2]
 
 console.log(education)
 
+//Creating objets
+
+let name = 'oscar';
+let age = 32 ;
+
+obj = {name:name, age : age};
+console.log(obj)
+
+obj2 = {name,age}
+console.log(obj2)
+
+
+////////////defin functions xD
+const names = [
+    {name: 'Oscar' ,age:32},
+    {name: 'Yesica' ,age:27}
+]
+
+let listOfNames = names.map(function(item){
+    console.log(item.name);
+
+
+})
+
+const names = [
+    {name: 'Oscar' ,age:32},
+    {name: 'Yesica' ,age:27}
+]
+
+let  listOfNames2 = names.map(item => console.log(item.name));
+
+//void function??
+listOfNames2
+
+
+
+const listOfNames3 =(name,age,county) =>{
+    return name
+}
+
+a = listOfNames3('hola',32,'mex')
+console.log(a)
+
+
+const listOfNames4 = name=>{
+    console.log(name)
+}
+listOfNames4('papi')
+
+
+const square = num =>num*num;
+
+console.log(square(2))
+
+//Promises..somethign gonna happen
+
+const helloPromise =() =>{
+    return new Promise((resolve,reject) =>{
+        if(true){
+            resolve('Hey!');
+        }else{
+            reject('ups');
+        }
+    }
+    )
+}
+
+helloPromise()
+.then(response =>console.log(response))
+.then(()=>console.log('Hola'))
+.catch(error =>console.log(error))
+
+
