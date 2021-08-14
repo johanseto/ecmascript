e1ad465c79c3f0f800a25064a74a82ec357f1433
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-13 22:14:35 -0500

    Set in es 8 async/wait hello world.

--- src/es8/index.js
+++ src/es8/index.js
@@ -37,3 +37,43 @@ console.log(string.padEnd(12,' -----'))
 const obj ={
     name:'Pedro',
 }
+
+
+//async /await
+
+const helloWorld = () =>{
+    return new Promise((resolve,reject) =>{
+        (false)
+        ? setTimeout(() =>resolve('Hello world'),3000)
+        : reject (new Error('test error'))
+    })
+}
+
+const helloAsync = async () =>{
+    const hello =  await helloWorld();
+    
+    console.log(hello);
+}
+
+helloAsync()
+
+//another way
+
+const helloWorld = () =>{
+    return new Promise((resolve,reject) =>{
+        (true)
+        ? setTimeout(() =>resolve('Hello world'),3000)
+        : reject (new Error('test error'))
+    })
+}
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch (error) {
+        console.log(error)
+    }
+}
+
+anotherFunction()
