4b5d11c9a9b3e786a042e6993e95b19b591917f7
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-13 21:52:32 -0500

    ecmascript 7
    includes includes function and exponent for number.

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+
+//Includes
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(7)) {
+    console.log('si se necuntra en 7')
+}else{
+    console.log('no se encuentra')
+}
+
+//exponent
+
+
+let base = 4;
+let exponent = 3;
+
+let result = base **exponent;
+console.log(result)
